97b5ae60f20aba5456334b19fcf46cc230d5ba9f
badmosh billa <billabdmosh@gmail.com>
2026-01-02 21:39:23 +0530

    added another file

---
+++ visit3.java
@@ -0,0 +1,21 @@
+
+class visit3{
+    static int[] a={1,2,3,4,5,6,7,8,9,10};
+    static int N=10;
+
+     static void visit(int i){
+        if(i>=N){
+            return;
+        }
+
+  
+        visit(2*i+1);
+        visit(2*i+2);
+        System.out.print(a[i]+" ");
+     }
+
+     public static void main(String [] args){
+        visit(0);
+     }
+
+}
